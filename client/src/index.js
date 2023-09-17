7907684d58c763fe3c9bbb2d25f29e93f8d2d91c
baotrong <levanbaotrong03@gmail.com>
2023-09-17 20:15:39 +0700

    Fix bug

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client'; // Import createRoot from "react-dom/client"
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
-ReactDOM.render(
+const root = document.getElementById('root');
+const reactRoot = createRoot(root); // Use createRoot to create a root for your React app
+
+reactRoot.render(
   <BrowserRouter>
     <App />
-  </BrowserRouter>,
-  document.getElementById('root')
+  </BrowserRouter>
 );
