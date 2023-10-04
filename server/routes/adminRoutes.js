043ff042ce462c4be1d9159bfbd4e281c004c1ed
Lê Vạn Bảo Trọng <32262183+chuanghiduoc@users.noreply.github.com>
2023-10-04 07:50:43 +0700

    Delete server directory

--- server/routes/adminRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const adminController = require('../controllers/adminController');
-const authMiddleware = require('../middleware/authMiddleware');
-
-router.get('/users-and-notes-info', authMiddleware, adminController.getUsersAndNotesInfo);
-router.post('/change-password/:userId', adminController.changePassword);
-
-module.exports = router;
